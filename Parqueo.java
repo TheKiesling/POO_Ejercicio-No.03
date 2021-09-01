6960eece8af8db29f6e122350c824881dba7dd11
TheKiesling <kie21581@uvg.edu.gt>
2021-08-31 19:22:21 -0600

    Create Parqueo.java
    Creación de las propiedades y métodos de la clase Parqueo

---
+++ Parqueo.java
@@ -0,0 +1,87 @@
+/*
+#      #    #######    ########   #######   #          #######   ##      #    #########
+#     #        #       #          #         #             #      # #     #    #
+#    #         #       #          #         #             #      #  #    #    #
+####           #       #####      #######   #             #      #   #   #    #    ####
+#    #         #       #                #   #             #      #    #  #    #       #
+#     #        #       #                #   #             #      #     # #    #       #
+#      #    ########   ########   #######   ########   #######   #      ##    #########
+*/
+
+public class Parqueo {
+    /** 
+    @author: José Pablo Kiesling Lange
+    Nombre del programa: Parqueo.java
+    @version: 
+        - Creación: 31/08/2021
+        - Última modificación: 31/08/2021
+
+    Clase que crea espacios de parqueos junto con las características de este y el vehículo que almacenan
+    */
+
+    //---------------------------PROPIEDADES-------------------------
+    private String tamano;
+    private Vehiculo vehiculo;
+    private String ubicacion;
+    private int horaIngreso; 
+    private int horasTotales;
+    private int vecesUsado = 0;
+
+    //---------------------------MÉTODOS-----------------------------
+
+    /****************************************************************
+     * instancia de los atributos del parqueo con los valores obtenidos a través de los parámetros. 
+     * @param tamano
+     * @param vehiculo
+     * @param ubicacion
+     * @param horaIngreso
+     */
+    public Parqueo(String tamano, Vehiculo vehiculo, String ubicacion, int horaIngreso){
+        this.tamano = tamano;
+        this.vehiculo = vehiculo;
+        this.ubicacion = ubicacion;
+        this.horaIngreso = horaIngreso;
+        vecesUsado++;
+    }
+    //***************************************************************
+
+    /****************************************************************
+     * Cálculo de las horas que estuvo el carro en el parqueo
+     * @param horaEgreso
+     */
+    public void calculoHorasTotales(int horaEgreso){
+        horasTotales = horaEgreso - horaIngreso;
+    }
+    //***************************************************************
+
+    /****************************************************************
+     * devuelve un arreglo con los datos Strings del parqueo: tamano y ubicacion
+     * @return datosParqueo
+     */
+    public String[] datosStrings(){
+        String[] datosParqueo = new String[2];
+        datosParqueo[0] = tamano; datosParqueo[1] = ubicacion;
+        return datosParqueo;
+    }
+    //***************************************************************
+
+    /****************************************************************
+     * devuelve el vehículo del parqueo
+     * @return vehiculo
+     */
+    public Vehiculo getVehiculo(){
+        return vehiculo;
+    }
+    //***************************************************************
+
+    /****************************************************************
+     * devuelve un arreglo con los datos ints del parqueo: horaIngreso, horasTotales y vecesUsado
+     * @return datosParqueo
+     */
+    public int[] datosInt(){
+        int[] datosParqueo = new int[3];
+        datosParqueo[0] = horaIngreso; datosParqueo[1] = horasTotales; datosParqueo[2] = vecesUsado;
+        return datosParqueo;
+    }
+    //***************************************************************
+}
